c45e4be757dc1a04ecc3de17fc588c057cff6647
Vira Murray <vira.avramenko@gmail.com>
2022-09-22 12:28:16 -0400

    Exercise 1

---
+++ Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesExercise
+{
+    public class Car
+    {
+        //Constructors 
+        public Car()  // a special member method that has the same name as its class
+        { 
+        }
+        //fields
+        //properties
+        //methods
+
+        //Create a Make property of type string that is public:
+        public string Make { get; set; }  //get = read; set = write 
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using ClassesExercise;
+
+var myCar = new Car();
+myCar.Make = "Ford";
+myCar.Model = "Focus";
+myCar.Year = 2019;
+
+Console.WriteLine(myCar.Make);
+Console.WriteLine(myCar.Model);
+Console.WriteLine(myCar.Year);
